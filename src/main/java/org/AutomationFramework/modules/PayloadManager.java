f24149c7f33a7d5babd892d60d23123710a2fb2f
Pavithra Dhanakotti <dhanakotipavithra26@gmail.com>
2024-04-29 11:50:14 +0530

    First commit

---
+++ src/main/java/org/AutomationFramework/modules/PayloadManager.java
@@ -0,0 +1,8 @@
+package org.AutomationFramework.modules;
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import org.AutomationFramework.pojos.*;
+
+public class PayloadManager {
+
+}
